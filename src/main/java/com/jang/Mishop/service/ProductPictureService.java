35cada5766638994674230384ad972e228205881
jqf <1484294001@qq.com>
2020-12-13 23:17:48 +0800

    第二次 完成product

--- src/main/java/com/jang/Mishop/service/ProductPictureService.java
+++ src/main/java/com/jang/Mishop/service/ProductPictureService.java
@@ -2,6 +2,9 @@ package com.jang.Mishop.service;
 
 import com.jang.Mishop.entity.ProductPicture;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.jang.Mishop.vo.ProductPic;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-12-12
  */
 public interface ProductPictureService extends IService<ProductPicture> {
-
+        List<ProductPicture> GetProductPic(ProductPic productPic);
 }
--- src/main/java/com/jang/Mishop/service/ProductService.java
+++ src/main/java/com/jang/Mishop/service/ProductService.java
@@ -1,7 +1,13 @@
 package com.jang.Mishop.service;
 
+import com.github.pagehelper.PageInfo;
 import com.jang.Mishop.entity.Product;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.jang.Mishop.vo.GetProByCatgIdReq;
+import com.jang.Mishop.vo.GetProByIdReq;
+import com.jang.Mishop.vo.GetProductByPage;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +18,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-12-12
  */
 public interface ProductService extends IService<Product> {
+    List<Product> getProductByCatgId(GetProByCatgIdReq getProByCatgIdReq);
 
+    List<Product> getHotProduct();
+
+    Product getProductByid(GetProByIdReq getProByIdReq);
+
+    PageInfo<Product> getProductByPage(GetProductByPage byPage);
 }
